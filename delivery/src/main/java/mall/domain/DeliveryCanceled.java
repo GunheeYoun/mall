68380ffcb3ddae014de4944f6478ff19066dce90
GunheeYoun <36002545+GunheeYoun@users.noreply.github.com>
2023-01-16 17:12:50 +0900

    commit

---
+++ delivery/src/main/java/mall/domain/DeliveryCanceled.java
@@ -0,0 +1,25 @@
+package mall.domain;
+
+import mall.domain.*;
+import mall.infra.AbstractEvent;
+import java.util.*;
+import lombok.*;
+
+
+@Data
+@ToString
+public class DeliveryCanceled extends AbstractEvent {
+
+    private Long id;
+    private Long orderId;
+    private String productName;
+    private Integer qty;
+    private String status;
+
+    public DeliveryCanceled(Delivery aggregate){
+        super(aggregate);
+    }
+    public DeliveryCanceled(){
+        super();
+    }
+}
